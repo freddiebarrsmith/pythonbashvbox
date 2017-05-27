5f38a4bb3ce22bd60692e6ec913fd2a4563a56b8
root <fredbarrsmith@gmail.com>
2017-05-27 20:21:15 +0000

    looks like it's subprocess that wins for now

--- listmachines.py
+++ listmachines.py
@@ -1,3 +1,12 @@
 import subprocess
 import os
+#http://www.saltycrane.com/blog/2011/04/how-use-bash-shell-python-subprocess-instead-binsh/
+#os.system()
+#def bash_command(cmd):
+#    subprocess.Popen(cmd, shell=True, executable='/bin/bash')
 
+#bash_command('VBoxManage list vms')
+
+#subprocess.call(["VBoxManage", "list", "vms"])
+strings = subprocess.check_output(["VBoxManage", "list", "vms"])
+print(strings)
