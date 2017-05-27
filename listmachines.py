bb5049838c0245b6b5c9ea9ea3cd7ec37455da71
root <fredbarrsmith@gmail.com>
2017-05-27 19:33:19 +0000

    starting to add object orientation

--- listmachines.py
+++ listmachines.py
@@ -1,7 +1,29 @@
 import subprocess
 import os
 import re
+
+class Machine(object):
+
+    def __init__(self, name, uuid):
+        self.name = name
+        self.uuid = uuid
+
+    def printuuid(self):
+        print self.uuid
+
+    def printname(self):
+        print self.name
+
+
+    def startvm(self):
+        subprocess.call(["VBoxManage", "startvm", self.name])
+
+    def stopvm(self):
+        subprocess.call(["VBoxManage", "poweroff", self.name])
+
 vmarray = []
+
+
 #http://www.saltycrane.com/blog/2011/04/how-use-bash-shell-python-subprocess-instead-binsh/
 #os.system()
 #def bash_command(cmd):
@@ -21,12 +43,12 @@ for line in strings.splitlines():
     line = line[1:]
     indexofsecondquote = line.index('\"')
     name = line[:indexofsecondquote]
-    vmarray.append(line)
+    vmarray.append(Machine(name, uuid))
     print name
 
 menuselect = raw_input("Which machine do you want to run ?(enter number)")
 menuselect = int(menuselect)
-print vmarray[menuselect-1]
-selectedvm = vmarray[menuselect-1]
+vmarray[menuselect-1].printname()
+
 #potentially clone before running
-subprocess.call(["VBoxManage", "startvm", selectedvm ])
+#subprocess.call(["VBoxManage", "startvm", selectedvm ])
