62004ac4b407d0a3480717f9a61715ce56222ae7
root <fredbarrsmith@gmail.com>
2017-05-29 19:01:41 +0000

    adding and updating hosts

--- listmachines.py
+++ listmachines.py
@@ -1,12 +1,16 @@
 import subprocess
 import os
 import re
+import time
+
+#class list(object):
 
 class Machine(object):
 
     def __init__(self, name, uuid):
         self.name = name
         self.uuid = uuid
+        self.clonename = name + str("cloned")
 
     def printuuid(self):
         print self.uuid
@@ -14,15 +18,55 @@ class Machine(object):
     def printname(self):
         print self.name
 
+    def printclonename(self):
+        print self.clonename
 
     def startvm(self):
-        subprocess.call(["VBoxManage", "startvm", self.name])
+        subprocess.call(["VBoxManage", "startvm", self.clonename])
 
     def stopvm(self):
-        subprocess.call(["VBoxManage", "poweroff", self.name])
+        subprocess.call(["VBoxManage", "controlvm", self.clonename, "poweroff"])
+
+    def clonevm(self):
+        subprocess.call(["VBoxManage", "clonevm", self.name, "--name", self.clonename, "--register"])
+
+    def deleteclone(self):
+        subprocess.call(["VBoxManage", "unregistervm", self.clonename, "--delete"])
+
+
+class Linux(Machine):
+    def __init__(self, name, uuid):
+        Machine.__init__(self, name, uuid)
+
+
+
+class Windows(Machine):
+    def __init__(self, name, uuid):
+        Machine.__init__(self, name, uuid)
+
+
+    def copyto(self):
+        subprocess.call(["VBoxManage", "startvm", self.name])
+
+    def copyfrom(self):
+        subprocess.call(["VBoxManage", "startvm", self.name])
+
+    def createdir(self):
+        subprocess.call(["vboxmanage", "guestcontrol", self.clonename, "createdir", "c://test", "--username", "pseudonym", "--password", "shifty"])
+#vboxmanage guestcontrol windowsxpcloned createdir "c://test2" --username pseudo --password shifty
+#vboxmanage guestcontrol windowsxpcloned createdir "c://test2" --username pseudonym --password shifty
+
+           #vboxmanage guestcontrol windowsxp createdir "c://test2" --username pseudo --password shifty
+
+
+            # vboxmanage guestcontrol "windowsxpcloned" copyto "/home/freddie/pythonbashvbox/testcopy.txt"
+            ## --target-directory "C:/test/" --username pseudo --password shifty --verbose
+
+
+#VBoxManage unregistervm  --delete
 
 vmarray = []
-
+clonearray = []
 
 #http://www.saltycrane.com/blog/2011/04/how-use-bash-shell-python-subprocess-instead-binsh/
 #os.system()
@@ -43,12 +87,33 @@ for line in strings.splitlines():
     line = line[1:]
     indexofsecondquote = line.index('\"')
     name = line[:indexofsecondquote]
-    vmarray.append(Machine(name, uuid))
+    vmarray.append(Windows(name, uuid))
     print name
 
 menuselect = raw_input("Which machine do you want to run ?(enter number)")
 menuselect = int(menuselect)
-vmarray[menuselect-1].printname()
+vmarray[menuselect-1].printuuid()
+vmarray[menuselect-1].printuuid()
+
+
+
+#
+vmarray[menuselect-1].deleteclone()
+
+vmarray[menuselect-1].clonevm()
+
+time.sleep(10)
+vmarray[menuselect-1].startvm()
+time.sleep(90)
+
+vmarray[menuselect-1].createdir()
+time.sleep(30)
+vmarray[menuselect-1].stopvm()
+
+vmarray[menuselect-1].deleteclone()
+
+
+#vmarray[menuselect-1].stopvm()
 
 #potentially clone before running
 #subprocess.call(["VBoxManage", "startvm", selectedvm ])
